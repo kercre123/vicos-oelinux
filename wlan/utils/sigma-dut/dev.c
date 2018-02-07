d5a1054917cfaf3ba3b258c74aa587d304bae385
Brian Chapados <chapados@anki.com>
2018-02-06 16:49:48 -0800

    [r00010.3] Update sigma-dut.git : f7e4994
    git-reposnap-release_tag: r00010.3
    git-reposnap-name: platform/vendor/qcom-opensource/wlan/utils/sigma-dut
    git-reposnap-prefix: wlan/utils/sigma-dut
    git-reposnap-fetch_url: https://source.codeaurora.org//platform/vendor/qcom-opensource/wlan/utils/sigma-dut.git
    git-reposnap-upstream: wlan-service.lnx.1.0.r11-rel
    git-reposnap-rev: f7e499414a9eeb03d8b370a52ee9743ee78971af
    
    commit f7e499414a9eeb03d8b370a52ee9743ee78971af
    Merge: 2630090 a7e6d3e
    Author: Linux Build Service Account <lnxbuild@localhost>
    Date:   Thu Sep 7 21:21:29 2017 -0600
    
        Merge a7e6d3ec3bb82899b8f0b9456f11f9a815c8d2ae on remote branch
    
        Change-Id: I834463b65860dd0f455dafd60d34ea3f33658c57

---
+++ wlan/utils/sigma-dut/dev.c
@@ -0,0 +1,124 @@
+/*
+ * Sigma Control API DUT (station/AP/sniffer)
+ * Copyright (c) 2011-2013, 2017, Qualcomm Atheros, Inc.
+ * All Rights Reserved.
+ * Licensed under the Clear BSD license. See README for more details.
+ */
+
+#include "sigma_dut.h"
+#include "miracast.h"
+
+
+static int cmd_dev_send_frame(struct sigma_dut *dut, struct sigma_conn *conn,
+			      struct sigma_cmd *cmd)
+{
+#ifdef MIRACAST
+	const char *program = get_param(cmd, "Program");
+
+	if (program && (strcasecmp(program, "WFD") == 0 ||
+			strcasecmp(program, "DisplayR2") == 0))
+		return miracast_dev_send_frame(dut, conn, cmd);
+#endif /* MIRACAST */
+
+	if (dut->mode == SIGMA_MODE_STATION ||
+	    dut->mode == SIGMA_MODE_UNKNOWN) {
+		sigma_dut_print(dut, DUT_MSG_DEBUG, "Convert "
+				"dev_send_frame to sta_send_frame");
+		return cmd_sta_send_frame(dut, conn, cmd);
+	}
+
+	if (dut->mode == SIGMA_MODE_AP) {
+		sigma_dut_print(dut, DUT_MSG_DEBUG, "Convert "
+				"dev_send_frame to ap_send_frame");
+		return cmd_ap_send_frame(dut, conn, cmd);
+	}
+
+#ifdef CONFIG_WLANTEST
+	sigma_dut_print(dut, DUT_MSG_DEBUG, "Convert dev_send_frame to "
+			"wlantest_send_frame");
+	return cmd_wlantest_send_frame(dut, conn, cmd);
+#else /* CONFIG_WLANTEST */
+	send_resp(dut, conn, SIGMA_ERROR,
+		  "errorCode,Unsupported dev_send_frame");
+	return 0;
+#endif /* CONFIG_WLANTEST */
+}
+
+
+static int cmd_dev_set_parameter(struct sigma_dut *dut, struct sigma_conn *conn,
+				 struct sigma_cmd *cmd)
+{
+	const char *device = get_param(cmd, "Device");
+
+	if (device && strcasecmp(device, "STA") == 0) {
+		sigma_dut_print(dut, DUT_MSG_DEBUG, "Convert "
+				"dev_set_parameter to sta_set_parameter");
+		return cmd_sta_set_parameter(dut, conn, cmd);
+	}
+
+	return -1;
+}
+
+
+static int cmd_dev_exec_action(struct sigma_dut *dut, struct sigma_conn *conn,
+			       struct sigma_cmd *cmd)
+{
+#ifdef MIRACAST
+	const char *program = get_param(cmd, "Program");
+
+	if (program && (strcasecmp(program, "WFD") == 0 ||
+			strcasecmp(program, "DisplayR2") == 0))
+		return miracast_dev_exec_action(dut, conn, cmd);
+#endif /* MIRACAST */
+
+       return -2;
+}
+
+
+static int cmd_dev_configure_ie(struct sigma_dut *dut, struct sigma_conn *conn,
+				struct sigma_cmd *cmd)
+{
+	const char *ie_name = get_param(cmd, "IE_Name");
+	const char *contents = get_param(cmd, "Contents");
+
+	if (!ie_name || !contents)
+		return -1;
+
+	if (strcasecmp(ie_name, "RSNE") != 0) {
+		send_resp(dut, conn, SIGMA_ERROR,
+			  "errorCode,Unsupported IE_Name value");
+		return 0;
+	}
+
+	free(dut->rsne_override);
+	dut->rsne_override = strdup(contents);
+
+	return dut->rsne_override ? 1 : -1;
+}
+
+
+static int req_intf(struct sigma_cmd *cmd)
+{
+	return get_param(cmd, "interface") == NULL ? -1 : 0;
+}
+
+
+static int req_intf_prog(struct sigma_cmd *cmd)
+{
+	if (get_param(cmd, "interface") == NULL)
+		return -1;
+	if (get_param(cmd, "program") == NULL)
+		return -1;
+	return 0;
+}
+
+
+void dev_register_cmds(void)
+{
+	sigma_dut_reg_cmd("dev_send_frame", req_intf_prog, cmd_dev_send_frame);
+	sigma_dut_reg_cmd("dev_set_parameter", req_intf_prog,
+			  cmd_dev_set_parameter);
+	sigma_dut_reg_cmd("dev_exec_action", req_intf_prog,
+			  cmd_dev_exec_action);
+	sigma_dut_reg_cmd("dev_configure_ie", req_intf, cmd_dev_configure_ie);
+}
