fb7742bff7378b23c191adb09711b85e82a146e2
Brian Chapados <chapados@anki.com>
2018-02-06 18:50:29 -0800

    Add simple build script for CI
    This is a simplified version of:
    https://github.com/anki/build-tools/blob/master/build-scripts/build-victor-os.sh
    
    All of the necessary code to build Linux kernel + userspace (boot, sysfs) is
    contained in this repo, so we no longer need to deal with an externally
    maintained tarball to build the OS.  We do still need additional code to
    generate image files for other SoC components as well as partition tables.

---
+++ poky/build/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Build starting at `date`"
+
+SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
+TOPLEVEL=$(cd "${SCRIPT_PATH}/../.." && pwd)
+
+# remove any existing artifacts
+pushd $TOPLEVEL/poky
+
+pushd build
+# known build artifact dirs
+rm -rf bitbake.lock cache downloads  sstate-cache  tmp-glibc
+
+# remove generated files under build/conf
+git clean -xddf -- ./conf
+popd
+
+# Disable check for unset variables (it will make the bitbake scripts exit)
+set +u
+source build/conf/set_bb_env.sh
+build-8009-robot-image
+
+# Re-enable check for unset variables
+set -u
+
+echo "Build finished at `date`"
+
+popd
