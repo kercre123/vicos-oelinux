81f4e90f2ba0b4cad34a135069cd529023fd5bb5
jean-pierre <jean-pierre@anki.com>
2018-02-09 17:54:11 -0800

    VIC-826 - Fix get_bblayers.py
    The change of get_bblayers.py to include meta-skeleton was
    overlooked. This fix the ommission

--- poky/build/conf/get_bblayers.py
+++ poky/build/conf/get_bblayers.py
@@ -21,7 +21,7 @@ def getLayerPaths(target,  fnexpr) :
     retList = []
     for file in os.listdir(target) :
         if (fnmatch.fnmatch(file, fnexpr) and not
-             (fnmatch.fnmatch(file, "meta-skeleton") or
+             (fnmatch.fnmatch(file, "meta-xskeleton") or
              fnmatch.fnmatch(file, "meta-selftest") or
              fnmatch.fnmatch(file, "meta-yocto") or
              fnmatch.fnmatch(file, "meta-yocto-bsp"))):
