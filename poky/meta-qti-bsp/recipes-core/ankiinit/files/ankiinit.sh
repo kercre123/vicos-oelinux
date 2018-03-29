8311455664d171bae88ab366caee55aa59cf1c6e
daniel <daniel@anki.com>
2018-03-29 13:17:02 -0700

    Restore debugability, ssh, adb, etc.
    This reverts commit 542a836f6efd59dd8f3a454ae249e623b33adf4d.

--- poky/meta-qti-bsp/recipes-core/ankiinit/files/ankiinit.sh
+++ poky/meta-qti-bsp/recipes-core/ankiinit/files/ankiinit.sh
@@ -48,7 +48,9 @@ fi
 
 if $HAVE_EMR; then
   echo $SERIALNO > /sys/class/android_usb/android0/iSerial
+  echo 2 1 w $SERIALNO | /system/bin/display
 fi
+echo 2 2 w `getprop persist.anki.robot.name` | /system/bin/display
 
 mkdir -p /data/misc/camera
 exit 0
