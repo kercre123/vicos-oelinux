a654c42432bf3aa5c340e02bbc039d427f14e00a
Adam Shelly <ashelly@anki.com>
2018-04-23 17:16:02 -0700

    VIC-2226: animfail file

---
+++ poky/meta-anki/recipes/rampost/files/animfail.c
@@ -0,0 +1,58 @@
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "rampost.h"
+#include "error_565.h"
+
+/******** TIMING UTILITIES ********/
+
+#define NSEC_PER_SEC  ((uint64_t)1000000000)
+#define NSEC_PER_MSEC ((uint64_t)1000000)
+#define NSEC_PER_USEC (1000)
+
+
+void microwait(long microsec)
+{
+  struct timespec time;
+  uint64_t nsec = microsec * NSEC_PER_MSEC;
+  time.tv_sec =  nsec / NSEC_PER_SEC;
+  time.tv_nsec = nsec % NSEC_PER_SEC;
+  nanosleep(&time, NULL);
+}
+
+
+/*********** ANIMFAIL IMPLEMENTATION **************/
+
+void exit_cleanup(void)
+{
+  lcd_gpio_teardown();
+}
+
+int error_exit(RampostErr err) {
+  exit_cleanup();
+  exit(err);
+}
+
+
+int main(int argc, const char* argv[]) {
+  bool success = false;
+
+  lcd_set_brightness(5);
+
+  lcd_gpio_setup();
+  lcd_spi_init();
+
+  lcd_device_reset();
+  lcd_device_init();
+
+  lcd_draw_frame2(error_565, error_565_len);
+
+  exit_cleanup();
+
+  return 0;
+}
