da1ad0b93edc27ca1d5e1d45b59ec081fbdeea92
Brian Chapados <chapados@anki.com>
2018-05-01 11:59:53 -0700

    VIC-2714 Support different commands in build.sh (#159)
    We already define several build_type+build_flavor combinations via shell functions
    in build/conf/set_bb_env.sh.  This change simply exposes those functions, or raw
    bitbake commands to the build.sh script, so that we can invoke these builds from our
    continuous integration systems and collect artifacts.
    
    This is required in order to setup different build configs for Release:Factory
    and Dev:Prod

--- poky/build/build.sh
+++ poky/build/build.sh
@@ -2,6 +2,46 @@
 
 set -e
 
+SCRIPT_NAME=$(basename $0)
+
+# defaults
+VERBOSE=0
+BUILD_COMMAND=("build-victor-robot-perf-image")
+
+function usage() {
+    echo "$SCRIPT_NAME [OPTIONS] <bitbake build command/function> (default: ${BUILD_COMMAND[*]})"
+    echo "  -h          print this message"
+    echo "  -v          print verbose output"
+}
+
+while getopts ":hv" opt; do
+    case $opt in
+        h)
+            usage
+            exit 1
+            ;;
+        v)
+            VERBOSE=1
+            ;;
+        :)
+            echo "Options -${OPTARG} requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ $VERBOSE -eq 1 ]; then
+    set -x
+fi
+
+# Move past getops args
+shift $(($OPTIND - 1))
+
+if [ $# -gt 0 ]; then
+    BUILD_COMMAND=$*
+fi
+
 echo "Build starting at `date`"
 
 SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
@@ -26,7 +66,7 @@ source build/conf/set_bb_env.sh
 export ANKI_BUILD_VERSION
 export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE ANKI_BUILD_VERSION"
 
-build-victor-robot-perf-image
+${BUILD_COMMAND[*]}
 
 popd
 
