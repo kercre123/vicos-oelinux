2da151e3881ddbf0ec085313ff3f3030a97fcee8
Alexander Kaplan <alexander@anki.com>
2018-08-21 11:14:18 -0700

    VIC-3034: use random key instead of serial_number

--- poky/meta-anki/recipes/user-data-locker/files/user-data-locker.c
+++ poky/meta-anki/recipes/user-data-locker/files/user-data-locker.c
@@ -1,27 +1,75 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
     int r=0;
-    char *vault_path="/sys/devices/soc0/serial_number";
+    FILE *fp=NULL;
 
-    int key_length=9;
+    // WARNING: switchboard uses the first 256kb of the switchboard partition
+    //          user-data-locker is using the last 256kb
+    char *vault_path="/dev/block/bootdevice/by-name/switchboard";
+    unsigned int vault_size=256*1024;
+
+    int key_length=65;
     char key[key_length];
 
-    FILE *fp;
+    char *rng_path="/dev/urandom";
 
-    fp = fopen(vault_path,"r");
-    if (fp==NULL) {
-        fprintf(stderr,"%s: ERROR: cannot open %s\n",argv[0],vault_path);
-        return 1;
+    if(argc>1 && !strcmp(argv[1],"reset")) {
+        // generate new key;
+        fp = fopen(rng_path,"rb");
+        if (fread(key,sizeof(char),key_length,fp)!=key_length) {
+            fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],key_length,rng_path);
+            r=4;
+            goto close_file;
+        }
+        fclose(fp);
+
+
+        // write key to vault
+        fp = fopen(vault_path,"wb");
+        if (fp==NULL) {
+            fprintf(stderr,"%s: ERROR: cannot open %s\n",argv[0],vault_path);
+            return 1;
+        }
+
+        if (fseek(fp,-vault_size,SEEK_END)) {
+            fprintf(stderr,"%s: ERROR: cannot set stream position to -%d bytes from SEEK_END\n",argv[0],key_length);
+            r=2;
+            goto close_file;
+        }
+
+        if (fwrite(key,sizeof(char),key_length,fp)!=key_length) {
+            fprintf(stderr,"%s: ERROR: cannot write %d bytes to %s\n",argv[0],key_length,vault_path);
+            r=3;
+            goto close_file;
+        }
+        
+    } else {
+        // read key from vault
+
+        fp = fopen(vault_path,"rb");
+        if (fp==NULL) {
+            fprintf(stderr,"%s: ERROR: cannot open %s\n",argv[0],vault_path);
+            return 1;
+        }
+
+        if (fseek(fp,-vault_size,SEEK_END)) {
+            fprintf(stderr,"%s: ERROR: cannot set stream position to -%d bytes from SEEK_END\n",argv[0],key_length);
+            r=2;
+            goto close_file;
+        }
+
+        if (fread(key,sizeof(char),key_length,fp)!=key_length) {
+            fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],key_length,vault_path);
+            r=4;
+            goto close_file;
+        }
+
     }
 
-    if (fread(key,sizeof(char),key_length,fp)!=key_length) {
-        fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],key_length,vault_path);
-        r=2;
-        goto close_file;
-    }
-
+    //output key
     fwrite(key,sizeof(char),key_length,stdout);
 
 close_file:
