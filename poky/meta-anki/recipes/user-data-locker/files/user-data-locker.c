308cbebda7ebdbd3721d0d4873a9245b8f846dd9
Alexander Kaplan <alexander@anki.com>
2018-08-21 18:23:59 -0700

    VIC-3034: Encrypt LUKS key using soc serial number

--- poky/meta-anki/recipes/user-data-locker/files/user-data-locker.c
+++ poky/meta-anki/recipes/user-data-locker/files/user-data-locker.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+#define DEBUG 0
+
+void encrypt(int length, char *clear, char *encrypted, int key_length, char *key)
+{
+    int i,j;
+    for (i=0, j=0; i<length; i++, j++) {
+        if (j>=key_length) {
+            j=0;
+        }
+        encrypted[i] = clear[i] ^ key[j]; 
+    }
+}
+void decrypt(int length, char *clear, char *encrypted, int key_length, char *key)
+{
+    int i,j;
+    for (i=0, j=0; i<length; i++, j++) {
+        if (j>=key_length) {
+            j=0;
+        }
+        clear[i] = encrypted[i] ^ key[j]; 
+    }
+}
+
+#if DEBUG
+void hexdump(int length, char *buf)
+{
+    for(int i=0; i<length; i++) {
+        fprintf(stderr,"%02x",buf[i]);
+    }
+}
+#endif
+
 int main(int argc, char** argv)
 {
     int r=0;
@@ -12,20 +44,52 @@ int main(int argc, char** argv)
     unsigned int vault_size=256*1024;
 
     int key_length=65;
-    char key[key_length];
+    char clear_key[key_length];
+    char encrypted_key[key_length];
+   
+    char *serial_path="/sys/devices/soc0/serial_number";
+    int serial_length=9;
+    char serial[serial_length];
 
     char *rng_path="/dev/urandom";
+
+    fp = fopen(serial_path,"rb");
+    if (fread(serial,sizeof(char),serial_length,fp)!=serial_length) {
+        fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],serial_length,serial_path);
+        r=5;
+        goto close_file;
+    }
+    fclose(fp);
+
+#if DEBUG
+    fprintf(stderr,"serial: ");
+    fwrite(serial,sizeof(char),serial_length,stderr);
+    fprintf(stderr,"\n");
+#endif
 
     if(argc>1 && !strcmp(argv[1],"reset")) {
         // generate new key;
         fp = fopen(rng_path,"rb");
-        if (fread(key,sizeof(char),key_length,fp)!=key_length) {
+        if (fread(clear_key,sizeof(char),key_length,fp)!=key_length) {
             fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],key_length,rng_path);
             r=4;
             goto close_file;
         }
         fclose(fp);
+        
+#if DEBUG
+        fprintf(stderr,"generated clear key: ");
+        hexdump(key_length,clear_key);
+        fprintf(stderr,"\n");
+#endif
 
+        encrypt(key_length, clear_key, encrypted_key, serial_length, serial);
+        
+#if DEBUG
+        fprintf(stderr,"encrypted key: ");
+        hexdump(key_length,encrypted_key);
+        fprintf(stderr,"\n");
+#endif
 
         // write key to vault
         fp = fopen(vault_path,"wb");
@@ -40,7 +104,7 @@ int main(int argc, char** argv)
             goto close_file;
         }
 
-        if (fwrite(key,sizeof(char),key_length,fp)!=key_length) {
+        if (fwrite(encrypted_key,sizeof(char),key_length,fp)!=key_length) {
             fprintf(stderr,"%s: ERROR: cannot write %d bytes to %s\n",argv[0],key_length,vault_path);
             r=3;
             goto close_file;
@@ -61,16 +125,30 @@ int main(int argc, char** argv)
             goto close_file;
         }
 
-        if (fread(key,sizeof(char),key_length,fp)!=key_length) {
+        if (fread(encrypted_key,sizeof(char),key_length,fp)!=key_length) {
             fprintf(stderr,"%s: ERROR: cannot read %d bytes from %s\n",argv[0],key_length,vault_path);
             r=4;
             goto close_file;
         }
 
+#if DEBUG
+        fprintf(stderr,"read encrypted key: ");
+        hexdump(key_length,encrypted_key);
+        fprintf(stderr,"\n");
+#endif
+
+        decrypt(key_length, clear_key, encrypted_key, serial_length, serial);
+
+#if DEBUG
+        fprintf(stderr,"decrypted clear key: ");
+        hexdump(key_length,clear_key);
+        fprintf(stderr,"\n");
+#endif
+
     }
 
     //output key
-    fwrite(key,sizeof(char),key_length,stdout);
+    fwrite(clear_key,sizeof(char),key_length,stdout);
 
 close_file:
     fclose(fp);
