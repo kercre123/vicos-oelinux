5fc3927d8df39378997afe5be2413b348f8fc43b
Mathew Prokos <mprokos@anki.com>
2018-09-14 11:11:02 -0700

    VIC-5407: Add open source release generation script
    This patch adds the opensource.sh script which can be used to generate the open
    source package including build instructions.

---
+++ opensource.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git clean -xffd
+cp -rf poky opensource/
+rm -rf opensource/poky/*-prop
+rm -rf opensource/poky/meta-anki
+
+cp -rf android_compat opensource/
+cp -rf kernel opensource/
+cp -rf qcom-opensource opensource/
+cp -rf system opensource/
+cp -rf frameworks opensource/
+cp -rf filesystems opensource/
+cp -rf mdm-init opensource/
+
+mkdir -p opensource/hardware
+cp -rf hardware/libhardware opensource/hardware
+
+mkdir -p opensource/external
+cp -rf external/bluetooth opensource/external
+cp -rf external/bspatch opensource/external
+cp -rf external/libselinux opensource/external
+cp -rf external/libunwind opensource/external
+cp -rf external/paycheck opensource/external
+cp -rf external/safe-iop opensource/external
+
+mkdir -p opensource/bootable/
+cp -rf bootable/bootloader opensource/bootable/
+
+mkdir -p opensource/mdm-ss-mgr/
+cp -rf mdm-ss-mgr/reboot-daemon opensource/mdm-ss-mgr/
+
+mkdir -p opensource/display/
+cp -rf display/libdrm opensource/display/
+
+tar -cvzf opensource.tgz opensource
+git clean -xfd opensource/
