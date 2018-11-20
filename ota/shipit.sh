7e154120438dd75c8b5e3834ec09caec82f4f3fc
Brian Chapados <chapados@anki.com>
2018-11-19 17:45:37 -0800

    Update shipit script to use version file artifact (#496)
    - Add param for changing build config id and set default to Vicos RC shipping build
    
    - Read ota_version.txt file to get UPDATE_VERSION for the OTA file
    
    Based on commit 42adbd64725fdca4c2d9cad4f1e61d16c8ad42e0

--- ota/shipit.sh
+++ ota/shipit.sh
@@ -13,11 +13,13 @@ function usage()
     echo "  -v                      Print verbose output"
     echo "  -o OUTPUT_DIR           Download artifacts to OUTPUT_DIR"
     echo "  -u TC_USER              Teamcity username"
+    echo "  -c TC_BUILD_CONFIG_ID          Teamcity build config identifier"
 }
 
 
 : ${VERBOSE:=0}
 : ${OUTPUT_DIR:="${SCRIPT_PATH}/../_build/tc-artifacts"}
+: ${TC_BUILD_CONFIG_ID:="Vicos_ReleaseCandidate_ReleaseCandidate_BuildShipping"}
 TC_USER=
 TC_PASS=
 
@@ -26,7 +28,7 @@ function logv()
     if [ $VERBOSE -eq 1 ]; then echo "$1"; fi
 }
 
-while getopts ":o:u:vh" opt; do
+while getopts ":o:u:c:vh" opt; do
     case $opt in
         h)
             usage
@@ -40,6 +42,9 @@ while getopts ":o:u:vh" opt; do
             ;;
         u)
             TC_USER="${OPTARG}"
+            ;;
+        c)
+            TC_BUILD_CONFIG_ID="${OPTARG}"
             ;;
     esac
 done
@@ -70,13 +75,14 @@ TC_AUTH="${TC_USER}:${TC_PASS}"
 ARTIFACTS=(
     apq8009-robot-boot.img.nonsecure
     apq8009-robot-sysfs.ext4
+    ota_version.txt
 )
 
 mkdir -p "${OUTPUT_DIR}"
 pushd ${OUTPUT_DIR}
 
-for IMG in "${ARTIFACTS[@]}"; do
-    URL="https://build.ankicore.com/repository/download/Vicos_Release/${BUILD_NUMBER}/${IMG}"
+for F in "${ARTIFACTS[@]}"; do
+    URL="https://build.ankicore.com/repository/download/${TC_BUILD_CONFIG_ID}/${BUILD_NUMBER}/${F}"
     logv "fetch ${URL}"
     curl \
         -O \
@@ -84,12 +90,14 @@ for IMG in "${ARTIFACTS[@]}"; do
         "${URL}"
 done
 
+: ${UPDATE_VERSION=`cat ota_version.txt`}
+
 popd
 
 logv "prodsign boot.img"
 make prodsign \
     IMG_DIR="${OUTPUT_DIR}" \
-    UPDATE_VERSION="${BUILD_VERSION}" \
+    UPDATE_VERSION="${UPDATE_VERSION}" \
     ANKIDEV=0
 
 logv "make prod OTA image"
@@ -97,13 +105,13 @@ read -r -s -p "Enter pass phrase for OTA signing key: " OTA_KEY_PASS
 make all \
     IMG_DIR="${OUTPUT_DIR}" \
     OTA_KEY_PASS="${OTA_KEY_PASS}" \
-    UPDATE_VERSION="${BUILD_VERSION}" \
+    UPDATE_VERSION="${UPDATE_VERSION}" \
     ANKIDEV=0
 
 logv "rename upload file"
-ln ../_build/vicos-${BUILD_VERSION}.ota ../_build/${BUILD_VERSION}.ota
+ln ../_build/vicos-${UPDATE_VERSION}.ota ../_build/${UPDATE_VERSION}.ota
 
 logv "OTA manifest dump"
-tar -O -xf ../_build/${BUILD_VERSION}.ota manifest.ini
+tar -O -xf ../_build/${UPDATE_VERSION}.ota manifest.ini
 
-echo "Generated OTA file: ../_build/${BUILD_VERSION}.ota"
+echo "Generated OTA file: ../_build/${UPDATE_VERSION}.ota"
