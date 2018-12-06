50ad0837dcf70e24ecf2796cf77222775aa6a4b7
Patrick Doran <patrick.doran@anki.com>
2018-12-06 23:28:19 +0000

    Add application signal handling

--- camera/lib-legacy/QCamera2/stack/mm-camera-pdoran/src/application.cpp
+++ camera/lib-legacy/QCamera2/stack/mm-camera-pdoran/src/application.cpp
@@ -1,11 +1,43 @@
 #include "application.h"
 
 #include "camera.h"
+#include "event.h"
 
-// TODO: Remove, these are for debugging experiments
-#include <thread>
-#include <chrono>
-#include <iostream>
+#include <csignal>
+
+//======================================================================================================================
+// Signal and Global Exit Event setup
+static Event gEvent;
+
+static void handle_signal(int signum)
+{
+  gEvent.set();
+}
+
+static void setup_signal_handler()
+{
+  struct sigaction new_action, old_action;
+
+  new_action.sa_handler = handle_signal;
+  sigemptyset(&new_action.sa_mask);
+  new_action.sa_flags = 0;
+
+  (void) sigaction(SIGINT, NULL, &old_action);
+  if (old_action.sa_handler != SIG_IGN) {
+    (void) sigaction(SIGINT, &new_action, NULL);
+  }
+  (void) sigaction(SIGHUP, NULL, &old_action);
+  if (old_action.sa_handler != SIG_IGN) {
+    (void) sigaction(SIGHUP, &new_action, NULL);
+  }
+  (void) sigaction(SIGTERM, NULL, &old_action);
+  if (old_action.sa_handler != SIG_IGN) {
+    (void) sigaction(SIGTERM, &new_action, NULL);
+  }
+}
+
+//======================================================================================================================
+// Application Implementation
 
 Application::Application()
   : _camera(nullptr)
@@ -18,10 +50,18 @@ Application::~Application()
 
 int Application::exec(const Args& args)
 {
-  std::cerr<<"Hello, World!"<<std::endl;
+  // Setup signals for exiting
+  setup_signal_handler();
+
+  // Create and start a new camera
   _camera = std::unique_ptr<Camera>(new Camera());
   _camera->start();
-  std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  // Wait for an exit signal (e.g. CTRL+C)
+  gEvent.wait();
+
+  // Stop the camera
   _camera->stop();
+
   return 0;
 }
---
+++ camera/lib-legacy/QCamera2/stack/mm-camera-pdoran/src/event.h
@@ -0,0 +1,39 @@
+#ifndef _mm_pdoran_event_h_
+#define _mm_pdoran_event_h_
+
+#include <mutex>
+#include <condition_variable>
+
+/**
+ * @brief Synchronization object that allows a thread to signal to other threads that an event has happened.
+ */
+class Event
+{
+public:
+  Event() : _triggered(false), _mutex(), _condition()
+  {
+  }
+
+  void wait(){
+    std::unique_lock<std::mutex> lk(_mutex);
+    _condition.wait(lk,[&]{return this->_triggered; });
+  };
+
+  void set()
+  {
+    _triggered = true;
+    _condition.notify_all();
+  }
+
+  void reset()
+  {
+    _triggered = false;
+  }
+
+private:
+  volatile bool _triggered;
+  std::mutex _mutex;
+  std::condition_variable _condition;
+};
+
+#endif
