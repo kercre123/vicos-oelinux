38d483f4c85e43c59106bf2d02bbfc38aaf40669
Steven Walter <stevenrwalter@xxxxxxxxx>
2018-12-14 11:05:33 -0800

    VIC-4461: Apply low_latency tty patch 1/2
    original commit message:
        [PATCH v3 1/2] drivers/tty: refactor functions for flushing/queuing work
    
        Preparation for re-iplementing low-latency port support.  Replace queue_work
        and schedule_work with tty_buffer_queue_work, and calls to flush_work with
        tty_buffer_flush_work.  cancel_work_sync are replaced by tty_buffer_flush_work
        as well, because kthread workqueues don't have a method of cancellation other
        than to flush.  flush could potentially take longer, but we would already delay
        if the callback were in progress.  Since it's only used from the release /
        destroy path, the difference should be negligible.
    
    Signed-off-by: Steven Walter <stevenrwalter@xxxxxxxxx>
    Signed-off-by: Mathew Prokos <mprokos@anki.com>

--- kernel/msm-3.18/drivers/tty/tty_ldisc.c
+++ kernel/msm-3.18/drivers/tty/tty_ldisc.c
@@ -591,9 +591,9 @@ int tty_set_ldisc(struct tty_struct *tty, int ldisc)
 
 	/* Restart the work queue in case no characters kick it off. Safe if
 	   already running */
-	schedule_work(&tty->port->buf.work);
+	tty_buffer_queue_work(tty->port);
 	if (o_tty)
-		schedule_work(&o_tty->port->buf.work);
+		tty_buffer_queue_work(o_tty->port);
 
 	tty_unlock(tty);
 	return retval;
