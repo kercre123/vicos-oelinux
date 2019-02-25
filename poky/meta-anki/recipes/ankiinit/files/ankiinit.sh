c6f9e958308b0f7bd69b8a51e8801c8f04e69e78
Daniel Casner <daniel@anki.com>
2019-02-25 11:52:57 -0800

    EMR enhancements for Whiskey support * Add emr-cat option to dump the entire structure * Expose HW_VER and MODEL from EMR as properties

--- poky/meta-anki/recipes/ankiinit/files/ankiinit.sh
+++ poky/meta-anki/recipes/ankiinit/files/ankiinit.sh
@@ -43,6 +43,10 @@ do
     sleep 1;
 done
 
+# Put the model into a property
+setprop ro.hw.ver `/bin/emr-cat v`
+setprop ro.model `/bin/emr-cat m`
+
 if [ $HAVE_EMR -eq 0 ]; then
   echo $SERIALNO > /sys/class/android_usb/android0/iSerial
 fi
