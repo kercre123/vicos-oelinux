c323f73914e17e0545e03e13f549c9286745d4fa
Grant Olson <grant@digitaldreamlabs.com>
2020-09-27 13:35:56 -0400

    When generating the unique ssh user key for the robot, wait until the robot has been configured with its name to start processing and then include the name in the file to make it obvious what key belongs to what robot.

--- poky/meta-ddl/recipes-extended/localsshuser/files/localsshuser.sh
+++ poky/meta-ddl/recipes-extended/localsshuser/files/localsshuser.sh
@@ -11,9 +11,9 @@
 # The /data partition is mounted via a service and not
 # immediately at boot via fstab. Wait until we see an
 # expected directory so we know it's mounted
-while [ ! -d "/data/etc" ];
+while [ -z "`getprop anki.robot.name`" ];
 do
-    logger -t localsshuser "/data not mounted yet. Sleeping for 5 seconds..."
+    logger -t localsshuser "Robot doesn't have a unique name yet. Sleeping for 5 seconds..."
     sleep 5
 done
 
@@ -22,12 +22,14 @@ mkdir -m 0700 /data/ssh
 if [ ! -f "/data/ssh/authorized_keys" ];
 then
     logger -t localsshuser "No Authorized Keys file, Creating..."
-    if [ ! -f "/data/ssh/id_rsa_victor" ]
+
+    ROBOT_ID=`getprop anki.robot.name | tr ' ' '-'`
+    if [ ! -f "/data/ssh/id_rsa_${ROBOT_ID}" ]
     then
         logger -t localsshuser "No Ssh key, generating..."
-        ssh-keygen -t rsa -f /data/ssh/id_rsa_victor -q -N ""
+        ssh-keygen -t rsa -f /data/ssh/id_rsa_${ROBOT_ID} -q -N ""
     fi
-    cat /data/ssh/id_rsa_victor.pub > /data/ssh/authorized_keys
+    cat /data/ssh/id_rsa_${ROBOT_ID}.pub > /data/ssh/authorized_keys
     echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDL5Z2nswVF0riBrtku1SdxC31tkwBbdc32kmZaVj6ItqxROelra/gNW4C8pBVT0Fka0sSiab8hvDjrkd2MH/WgvDwrSoNMky49uZZOvp0uNRcn7M9Jb/W0DJl3Fxm1R//Zt4bMSBjZDrwthgjYUz2vgEulQALgK+VuoY1WKIRkrrUebyximcYqeg4tDIvzbMWfuhQVkoW9ME0tsUyRSlBH5eQ6mjbgbLThaDOUtgCuLZaPUVlG1gAqfYDTQFyJRKgreCspKchssmKS5s0Uu7O6iOsqefZSPNAFHo33+LO7c0S5kPpClGTmT+hyCzK1HpzCWbTu7RRquJR/nyRhavPcc+zAAfsjE758h3rXuAQXWzqMc/1FAMm1sDRqyQkyNyTLAgHLVCgoDLcRDQfHBzhplqFxOFqXTq3UsQDcIv49M9Q5E5J8L4uH46Fcn7FyhYsG+Dc2DqWy1XUFLK6fyxIlD74FdKkdz3UXUvU2D942NZKKgq2qpxcAwbmRfUS5NTk= digital_dream_labs_dev_key" >> /data/ssh/authorized_keys
 fi
 
