9e40cf2218cb3d7c2d5b05acc6988600aa15468b
Grant T. Olson <grant@digitaldreamlabs.com>
2020-09-29 21:31:19 -0400

    Add perfoskr settings for OSKR unlocks

--- poky/build/conf/set_bb_env.sh
+++ poky/build/conf/set_bb_env.sh
@@ -26,7 +26,7 @@ then
 fi
 
 umask 022
-unset DISTRO MACHINE PRODUCT VARIANT FACTORY
+unset DISTRO MACHINE PRODUCT VARIANT FACTORY OSKR
 
 # OE doesn't want a set-gid directory for its tmpdir
 BT="./build/tmp-glibc"
@@ -94,6 +94,16 @@ function build-8009-robot-perf-image() {
   cdbitbake machine-robot-image
 }
 
+function build-8009-robot-perfoskr-image() {
+  unset_bb_env
+  export MACHINE=apq8009-robot
+  export DISTRO=msm-perf
+  export VARIANT=perf
+  export PRODUCT=robot
+  export OSKR="1"
+  cdbitbake machine-robot-image
+}
+
 function build-8009-robot-user-image() {
   unset_bb_env
   export MACHINE=apq8009-robot
@@ -124,6 +134,10 @@ function build-victor-robot-image() {
 
 function build-victor-robot-perf-image() {
   build-8009-robot-perf-image
+}
+
+function build-victor-robot-perfoskr-image() {
+  build-8009-robot-perfoskr-image
 }
 
 function build-victor-robot-user-image() {
@@ -182,7 +196,7 @@ rebake() {
 }
 
 unset_bb_env() {
-  unset DISTRO MACHINE PRODUCT VARIANT FACTORY
+  unset DISTRO MACHINE PRODUCT VARIANT FACTORY OSKR
 }
 
 # Find build templates from qti meta layer.
@@ -198,6 +212,6 @@ export TEMPLATECONF="meta-qti-bsp/conf"
 # (BBLAYERS is explicitly blocked from this within OE-Core itself, though...)
 # oe-init-build-env calls oe-buildenv-internal which sets
 # BB_ENV_EXTRAWHITE, append our vars to the list
-export BB_ENV_EXTRAWHITE="${BB_ENV_EXTRAWHITE} DL_DIR PRODUCT VARIANT FACTORY DEV QSN"
+export BB_ENV_EXTRAWHITE="${BB_ENV_EXTRAWHITE} DL_DIR PRODUCT VARIANT FACTORY DEV QSN OSKR"
 
 list-build-commands
