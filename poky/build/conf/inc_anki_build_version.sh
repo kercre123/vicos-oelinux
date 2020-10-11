d0ee76e4798794ebcd85d8c9892d9d33aa4de3b5
Grant Olson <grant@digitaldreamlabs.com>
2020-10-11 20:17:18 +0000

    Helper script for ddl automated builds

---
+++ poky/build/conf/inc_anki_build_version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Export settings so we get the build version in the app.
+#
+
+# Find where the global conf directory is...
+scriptdir="$(dirname "${BASH_SOURCE}")"
+# Find where the workspace is...
+WS=$(readlink -f $scriptdir/../../..)
+CONF_DIR=$WS/poky/build/conf
+BUILD_VERSION_FILE=$CONF_DIR/anki_build_version.txt
+
+if [[ -f "$BUILD_VERSION_FILE" ]];then
+    echo "FOUND $BUILD_VERSION_FILE"
+    CURRENT_BUILD_VERSION=$(cat $BUILD_VERSION_FILE)
+else
+    echo "DIDN'T FIND $BUILD_VERSION_FILE"
+    CURRENT_BUILD_VERSION=6000
+fi
+
+NEXT_BUILD_VERSION=$(expr "$CURRENT_BUILD_VERSION" + 1)
+echo $NEXT_BUILD_VERSION > $BUILD_VERSION_FILE
+echo Updated build version to $NEXT_BUILD_VERSION ...
