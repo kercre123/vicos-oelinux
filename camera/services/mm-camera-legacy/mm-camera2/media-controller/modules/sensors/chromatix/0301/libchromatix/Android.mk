18b8cd1f14f9e210a95098a56c36561e32bfef04
Grant T. Olson <grant@digitaldreamlabs.com>
2022-01-30 12:22:13 -0500

    Move to bf2253l camera

--- camera/services/mm-camera-legacy/mm-camera2/media-controller/modules/sensors/chromatix/0301/libchromatix/Android.mk
+++ camera/services/mm-camera-legacy/mm-camera2/media-controller/modules/sensors/chromatix/0301/libchromatix/Android.mk
@@ -1,6 +1,7 @@
 ifeq ($(TARGET_ARCH),$(filter $(TARGET_ARCH),arm arm64))
   ifeq ($(call is-board-platform-in-list, msm8909),true)
-    include $(call all-named-subdir-makefiles, chromatix_ov5648_q5v22e)
+    # include $(call all-named-subdir-makefiles, chromatix_ov5648_q5v22e)
+    include $(call all-named-subdir-makefiles, chromatix_bf2253L)
   else
     include $(call all-subdir-makefiles)
   endif
