b2acb2233cabccb0a18faf4b66d27b386ffb0159
Grant Olson <grant@digitaldreamlabs.com>
2022-03-09 20:12:18 +0000

    Fixes for build scripts. Force user to set AWS keys and get id correctly

--- poky/build/conf/build_40_inc_anki_build_version.sh
+++ poky/build/conf/build_40_inc_anki_build_version.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+[ -z ${AWS_ACCESS_KEY_ID+y} ] && echo "AWS_ACCESS_KEY_ID not defined" && exit 1
+[ -z ${AWS_SECRET_ACCESS_KEY+y} ] && echo "AWS_SECRET_ACCESS_KEY not defined" && exit 1
 #
 # Export settings so we get the build version in the app.
 #
@@ -11,13 +13,11 @@ scriptdir="$(dirname "${BASH_SOURCE}")"
 # Find where the workspace is...
 CONF_DIR=~/vicos-builds
 CONF_FILE=anki_build_version.txt
-REMOTE_URL=http://assets.digitaldreamlabs.com/vic/ufxTn3XGcVNK2YrF/
 OUT_DIR=s3://assets.digitaldreamlabs.com/vic/ufxTn3XGcVNK2YrF/
+BUILD_VERSION_FILE=${CONF_DIR}/${CONF_FILE}
 
 mkdir -p ${CONF_DIR}
-wget -P ${CONF_DIR} ${REMOTE_URL}${CONF_FILE}
-
-BUILD_VERSION_FILE=$CONF_DIR/anki_build_version.txt
+echo aws s3 cp  ${OUT_DIR}${CONF_FILE} ${BUILD_VERSION_FILE}
 
 if [[ -f "$BUILD_VERSION_FILE" ]];then
     echo "FOUND $BUILD_VERSION_FILE"
@@ -30,7 +30,6 @@ fi
 NEXT_BUILD_VERSION=$(expr "$CURRENT_BUILD_VERSION" + 1)
 echo $NEXT_BUILD_VERSION > $BUILD_VERSION_FILE
 
-export AWS_ACCESS_KEY_ID=AKIAVLI25QX5MKAPR4TE
-aws s3 mv ${BUILD_VERSION_FILE} ${OUT_DIR}
+echo aws s3 cp ${BUILD_VERSION_FILE} ${OUT_DIR}
 
 echo Updated build version to $NEXT_BUILD_VERSION ...
