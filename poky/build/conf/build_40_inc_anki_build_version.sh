22ed02e66b09bd1f4d1f5ddd19539c5767d4c33f
Grant T. Olson <grant@digitaldreamlabs.com>
2022-03-09 08:21:45 -0500

    modify Build task to retrieve latest build number in case we have multiple servers

--- poky/build/conf/build_40_inc_anki_build_version.sh
+++ poky/build/conf/build_40_inc_anki_build_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 #
 # Export settings so we get the build version in the app.
 #
@@ -8,7 +10,13 @@
 scriptdir="$(dirname "${BASH_SOURCE}")"
 # Find where the workspace is...
 CONF_DIR=~/vicos-builds
+CONF_FILE=anki_build_version.txt
+REMOTE_URL=http://assets.digitaldreamlabs.com/vic/ufxTn3XGcVNK2YrF/
+OUT_DIR=s3://assets.digitaldreamlabs.com/vic/ufxTn3XGcVNK2YrF/
+
 mkdir -p ${CONF_DIR}
+wget -P ${CONF_DIR} ${REMOTE_URL}${CONF_FILE}
+
 BUILD_VERSION_FILE=$CONF_DIR/anki_build_version.txt
 
 if [[ -f "$BUILD_VERSION_FILE" ]];then
@@ -21,4 +29,8 @@ fi
 
 NEXT_BUILD_VERSION=$(expr "$CURRENT_BUILD_VERSION" + 1)
 echo $NEXT_BUILD_VERSION > $BUILD_VERSION_FILE
+
+export AWS_ACCESS_KEY_ID=AKIAVLI25QX5MKAPR4TE
+aws s3 mv ${BUILD_VERSION_FILE} ${OUT_DIR}
+
 echo Updated build version to $NEXT_BUILD_VERSION ...
